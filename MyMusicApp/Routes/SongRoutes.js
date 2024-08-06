9457f7fc8aeaccdd2c04e07ff21dd13c5e379647
Lucas <binderlucas356@gmail.com>
2024-08-06 11:30:05 -0500

    Initial commit

---
+++ MyMusicApp/Routes/SongRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const songController = require('../controllers/songController');
+
+// Defina suas rotas para músicas aqui
+// Exemplo: router.get('/songs', songController.listSongs);
+
+module.exports = router;
